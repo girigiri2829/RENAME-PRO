4646199b33a4f8542ddb73d6945b3232f10b4dc3
Sridharan <128954293+girigiri2829@users.noreply.github.com>
2024-11-23 18:20:34 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,20 +1,20 @@
 import os, re
 
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-API_ID = int(os.environ.get("API_ID", ""))
-API_HASH = os.environ.get("API_HASH", "")
-OWNER = int(os.environ.get("OWNER", ""))
-BOT_USERNAME = os.environ.get('BOT_USERNAME', "")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "6443893219:AAERHhKv09MfBuQsA-6z9Z5hZVfAsn_eMRw")
+API_ID = int(os.environ.get("API_ID", "23049826"))
+API_HASH = os.environ.get("API_HASH", "4a4216f089ce68a3ce2c8b9b9a6fa79a")
+OWNER = int(os.environ.get("OWNER", "1781188088"))
+BOT_USERNAME = os.environ.get('BOT_USERNAME', "GiSimpleRenameBot")
 
-FORCE_SUBS = os.environ.get("FORCE_SUBS", "HxBots")
-LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002031041556"))
+FORCE_SUBS = os.environ.get("FORCE_SUBS", "")
+LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002357303644"))
 
-DB_URL = os.environ.get("DB_URL", "")
-DB_NAME = os.environ.get("DB_NAME", "renamebot-premium")
+DB_URL = os.environ.get("DB_URL", "mongodb+srv://aman:hdhub4net@cluster0.f6fbxm4.mongodb.net/?retryWrites=true&w=majority")
+DB_NAME = os.environ.get("DB_NAME", "hdhub4net")
 
 STRING = os.environ.get("STRING", "")
-BOT_PIC = os.environ.get("BOT_PIC", "https://graph.org/file/cca849a2f63053fa3f622.jpg")
+BOT_PIC = os.environ.get("BOT_PIC", "https://graph.org/file/f67fe27635fadb47aafb4.jpg")
 
 
 
